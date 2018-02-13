f727094e3875e27aaebc58d7cb6b6498bad7908e
pablo <taddeiapablo23@gmail.com>
2018-02-13 00:33:17 -0300

    tercer commit
    mejorado y simplificado del codigo y falta terminar algunas cosas

--- beautifulsoup_sample/beautifulsoup_sample/beautifulsoup_sample.py
+++ beautifulsoup_sample/beautifulsoup_sample/beautifulsoup_sample.py
@@ -9,12 +9,13 @@ import json
 
 # clase categorias, que ofrece mercadolibre
 class Categoria(object):
-    id = ''
-    name = ''
+    url = ''
+    title = ''
 
 class Product(object):
     pass
 
+## clase utilizada para hacer scrapy de productos y servicios
 class Mercadolibre_soup(object):
     # metodo por la cual me traigo las categorias ofrecidad por mercadolibre
     def content_categorias(self):
@@ -28,40 +29,37 @@ class Mercadolibre_soup(object):
             categoria.title = al.a['title']
             categorias.append(categoria)
         return categorias
-    def categories_get(self):
-        response = requests.get('https://api.mercadolibre.com/sites/MLA/categories')
-        list = response.json()
-        categories = []
-        for x in list:
-            cate = Categoria()
-            cate.name = x['name']
-            cate.id = x['id']
-            categories.append(cate)
-        for y in categories:
-            print(y.id + y.name)
+    def search_producs(self, link):
+        contents = urllib.urlopen(link)
+        bet = BeautifulSoup(contest, 'lxml')
+        all = bet.findAll('div', attrs={'class':'rowItem item item--stack new'})
+
 
 
 if __name__ == "__main__":
     # aqui creo una instancia de mercadolibre_soup
     # y llamo al metodo content_categorias para traerme las categorias que ofrece mercado libre
-    
-    cat = 'Notebooks'
-    producto = 'compac presario'
-
     mercado = Mercadolibre_soup()
-    categorias = mercado.content_categorias()
-    #mercado.categories_get()
-    for c in categorias:
-        if c.url.find(cat) != -1 :
-            print('contiene')
-        auxiliar = c.title.split('y')
 
-        if len(auxiliar) > 1:
-            if auxiliar[0].find(cat) != -1:
-                aux_url = c.url + auxiliar[0] + "-" + producto
-                print(aux_url)
-                break
-            elif auxiliar[1].find(cat) != -1:
-                aux_url = c.url + auxiliar[1] + "/" + producto
-                print(aux_url)
-                break
+    cat = 'Notebooks'
+    producto = 'compaq presario'
+
+    categorias = mercado.content_categorias()
+
+    for c in categorias:
+        if c.title.find(cat) != -1 :
+            final_url = c.url + producto
+            #print(final_url)
+            mercado.search_producs(final_url)
+            break
+    #for c in categorias:
+        #auxiliar = c.title.split('y')
+        #if len(auxiliar) > 1:
+            #if auxiliar[0].find(cat) != -1:
+                #aux_url = c.url + auxiliar[0] + "-" + producto
+                #print(aux_url)
+                #break
+            #elif auxiliar[1].find(cat) != -1:
+                #aux_url = c.url + auxiliar[1] + "/" + producto
+                #print(aux_url)
+                #break
